b419b5adba1df70c31f9b6736239ba8965b6e3a6
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-01 12:38:20 -0300

    docs: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,18 @@ async function bootstrap() {
 
   // Validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('PizzaFresh')
+    .setDescription('Aplicação para gestão das mesas de uma pizzaria')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('table')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
