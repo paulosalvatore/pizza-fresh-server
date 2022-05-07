98d6d95578fe53201d4eea0b35f0b28b7ffcdb09
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-07 15:47:57 -0300

    chore(table): implement not found error for `update`

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -12,7 +12,7 @@ export class TableService {
     return this.prisma.table.findMany();
   }
 
-  async findOne(id: string): Promise<Table> {
+  async findById(id: string): Promise<Table> {
     const record = await this.prisma.table.findUnique({ where: { id } });
 
     if (!record) {
@@ -22,13 +22,19 @@ export class TableService {
     return record;
   }
 
+  async findOne(id: string): Promise<Table> {
+    return this.findById(id);
+  }
+
   create(dto: CreateTableDto): Promise<Table> {
     const data: Table = { ...dto };
 
     return this.prisma.table.create({ data });
   }
 
-  update(id: string, dto: UpdateTableDto): Promise<Table> {
+  async update(id: string, dto: UpdateTableDto): Promise<Table> {
+    await this.findById(id);
+
     const data: Partial<Table> = { ...dto };
 
     return this.prisma.table.update({
