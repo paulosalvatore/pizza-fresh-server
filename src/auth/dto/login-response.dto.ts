38cc977e1ea011ad4416a68258581eed57b57698
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-11 12:08:26 -0300

    feat(auth): implement login

--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -4,7 +4,8 @@ import { User } from 'src/user/entities/user.entity';
 export class LoginResponseDto {
   @ApiProperty({
     description: 'JWT gerado pelo login',
-    example: 'TOKEN_GERADO_AUTOMATICAMENTE',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6InBhdWxvc2FsdmF0b3JlIiwiaWF0IjoxNjUyMjgxNjUwLCJleHAiOjE2NTIzNjgwNTB9._otkmKymFZ4kgxwAMdK5oXBDg6cK6_NXwCrP64IilS8',
   })
   token: string;
 
