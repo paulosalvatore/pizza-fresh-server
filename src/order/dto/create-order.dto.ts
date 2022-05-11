6cafd1306d6facd2ac2851e07c8a6be816865bd7
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-11 12:43:25 -0300

    feat(auth): implement logged user

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -4,13 +4,6 @@ import { IsInt, IsPositive, IsUUID, ValidateNested } from 'class-validator';
 import { CreateOrderProductDto } from './create-order-product.dto';
 
 export class CreateOrderDto {
-  @IsUUID()
-  @ApiProperty({
-    description: 'ID do usuário que está criando o pedido',
-    example: 'e1bc0c89-a319-44df-a6e9-db66fe7b956b',
-  })
-  userId: string;
-
   @IsInt()
   @IsPositive()
   @ApiProperty({
